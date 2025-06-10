6eb23def93a20282c2bff949460bb32dfda9295a
Julian Curalli <julian.curalli27@gmail.com>
2025-06-10 12:09:21 -0400

    added Scalar API docs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+import configureOpenAPI from "~/lib/configure-openapi.js";
 import createApp from "~/lib/create-app.js";
 
 const app = createApp();
+
+configureOpenAPI(app);
 
 export default app;
---
+++ src/lib/configure-openapi.ts
@@ -0,0 +1,23 @@
+import type { OpenAPIApp } from "~/lib/types.js";
+import { Scalar } from "@scalar/hono-api-reference";
+import packageJson from "../../package.json" with { type: "json" };
+
+export default function configureOpenAPI(app: OpenAPIApp) {
+  app.doc("/api-doc", {
+    openapi: "3.1.0",
+    info: {
+      version: packageJson.version,
+      title: "RHR Scouting API",
+    },
+  });
+
+  app.get("/api-reference", Scalar({
+    url: "/api-doc",
+    theme: "bluePlanet",
+    pageTitle: "RHR Scouting API Reference",
+    defaultHttpClient: {
+      targetKey: "js",
+      clientKey: "fetch",
+    },
+  }));
+}
